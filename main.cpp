ca1a88a09761b6e6f53a4a3b4b08ce9e81602c55
844958340405984 <ralbrecht@cedarville.edu>
2021-07-17 15:54:21 -0400

    6.7 Common Constants and Utility Functions

--- main.cpp
+++ main.cpp
@@ -1,41 +1,19 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittableList.h"
+#include "sphere.h"
 
 #include <iostream>
 
-double hitSphere(const point3 &center, double radius, const ray &r) {
-    // vector from center to point of the ray
-    vec3 oc = r.getOrigin() - center;
-    // (t^2) * b dot b + 2tb dot (A-C) + (A-C) dot (A-C) - r*2 = 0
-    // where t is variable
-    const auto a = r.getDirection().lengthSquared();
-    const auto half_b = dot(oc, r.getDirection());
-    const auto c = oc.lengthSquared() - radius * radius;
-    // discriminant of quadratic polynomial where a != 0 is b^2 - 4ac
-    const auto discriminant = half_b * half_b - a * c;
-    // positive (meaning two real solutions)
-    // negative (meaning no real solutions)
-    // zero (meaning one real solution)
-    // solving for t
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-}
-
-color rayColor(const ray &r) {
-    const point3 circleCenter = point3(0, 0, -1.0);
-    auto t = hitSphere(circleCenter, 0.5, r);
-    if (t > 0.0) {
-        // surface normal vector from hit point
-        vec3 N = unitVector(r.at(t) - circleCenter);
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color rayColor(const ray &r, const hittable &world) {
+    hitRecord rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
 
     const vec3 unitDirection = unitVector(r.getDirection());
-    t = 0.5 * (unitDirection.y() + 1.0);
+    const auto t = 0.5 * (unitDirection.y() + 1.0);
     // linear blend where t = 1 is blue, t = 0 is white, and in between is a blend.
     // formula is blendedValue=(1−t) * startValue+ t * endValue,
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -48,6 +26,11 @@ int main() {
     const double aspectRatio = 16.0 / 9.0;
     const int imageWidth = 400;
     const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
+    // World
+    hittableList world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
 
@@ -71,7 +54,7 @@ int main() {
             const auto u = double(x) / (imageWidth - 1);
             const auto v = double(y) / (imageHeight - 1);
             ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
-            color pixelColor = rayColor(r);
+            color pixelColor = rayColor(r, world);
             writeColor(std::cout, pixelColor);
         }
     }
