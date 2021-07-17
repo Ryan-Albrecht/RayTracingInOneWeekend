9cc9a8f3b42c14e579fe05c40e0eb494a579941e
844958340405984 <ralbrecht@cedarville.edu>
2021-07-17 16:07:31 -0400

    7.1 Some Random Number Utilities

--- rtweekend.h
+++ rtweekend.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 
@@ -19,6 +20,12 @@ const double pi = 3.1415926535897932385;
 
 inline double degreesToRadians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double randomDouble() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
 // Common Headers
