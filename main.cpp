a60b97aeb5ee1ab1891a2b3e2401e3ff9b61f176
844958340405984 <ralbrecht@cedarville.edu>
2021-07-18 16:56:18 -0400

    13.1 A Final Render

--- main.cpp
+++ main.cpp
@@ -7,6 +7,50 @@
 
 #include <iostream>
 
+hittableList randomScene() {
+    hittableList world;
+
+    const auto groundMaterial = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, groundMaterial));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            const auto chooseMaterial = randomDouble();
+            const point3 center(a + 0.9 * randomDouble(), 0.2, b + 0.9 * randomDouble());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphereMaterial;
+
+                if (chooseMaterial < 0.8) {
+                    // diffuse
+                    const auto albedo = color::random() * color::random();
+                    sphereMaterial = make_shared<lambertian>(albedo);
+                } else if (chooseMaterial < 0.95) {
+                    // metal
+                    const auto albedo = color::random(0.5, 1);
+                    const auto fuzz = randomDouble(0, 0.5);
+                    sphereMaterial = make_shared<metal>(albedo, fuzz);
+                } else {
+                    // glass
+                    sphereMaterial = make_shared<dielectric>(1.5);
+                }
+                world.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+            }
+        }
+    }
+
+    const auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    const auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    const auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 color rayColor(const ray &r, const hittable &world, int depth) {
     hitRecord rec;
 
@@ -36,37 +80,26 @@ int main() {
 
     // Image
 
-    const double aspectRatio = 16.0 / 9.0;
-    const int imageWidth = 400;
+    const double aspectRatio = 3.0 / 2.0;
+    const int imageWidth = 1200;
     const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
-    const int samplesPerPixel = 100;
+    const int samplesPerPixel = 500;
     const int maxDepth = 50;
 
     // World
 
-    const auto R = cos(pi / 4);
-    hittableList world;
-
-    const auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    const auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    const auto material_left = make_shared<dielectric>(1.5);
-    const auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    hittableList world = randomScene();
 
     // Camera
 
-    const point3 lookFrom(3, 3, 2);
-    const point3 lookAt(0, 0, -1);
+    const point3 lookFrom(13, 2, 3);
+    const point3 lookAt(0, 0, 0);
     const vec3 vup(0, 1, 0);
-    const auto distToFocus = (lookFrom - lookAt).length();
-    const auto aperture = 2.0;
+    const auto distToFocus = 10.0;
+    const auto aperture = 0.1;
+    const auto verticalFieldOfView = 20;
 
-    camera cam(lookFrom, lookAt, vup, 20, aspectRatio, aperture, distToFocus);
+    camera cam(lookFrom, lookAt, vup, verticalFieldOfView, aspectRatio, aperture, distToFocus);
 
     // Render to PPM (Portable Pix Map) image format
 
