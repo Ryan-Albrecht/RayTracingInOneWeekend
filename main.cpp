1a10048e6ee85a37ec801836847b2e785f32a11e
844958340405984 <ralbrecht@cedarville.edu>
2021-07-18 03:05:47 -0400

    10.5 Modeling a Hollow Glass Sphere

--- main.cpp
+++ main.cpp
@@ -53,6 +53,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, materialGround));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, materialCenter));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, materialLeft));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, materialRight));
 
     // Camera
