879429416ad6467e6f5e169ace4531dd2a22ea9d
844958340405984 <ralbrecht@cedarville.edu>
2021-07-18 14:38:50 -0400

    11.2 Positioning and Orienting the Camera

--- camera.h
+++ camera.h
@@ -5,22 +5,24 @@
 class camera {
 public:
     // verticalFieldOfView is in degrees
-    camera(double verticalFieldOfView, double aspectRatio) {
+    camera(point3 lookFrom, point3 lookAt, vec3 vup, double verticalFieldOfView, double aspectRatio) {
         const auto theta = degreesToRadians(verticalFieldOfView);
         const auto height = tan(theta / 2);
         const auto viewportHeight = 2.0 * height;
         const auto viewportWidth = aspectRatio * viewportHeight;
 
-        const auto focalLength = 1.0;
+        const auto w = unitVector(lookFrom - lookAt);
+        const auto u = unitVector(cross(vup, w));
+        const auto v = cross(w, u);
 
-        origin = point3(0, 0, 0);
-        horizontal = vec3(viewportWidth, 0, 0);
-        vertical = vec3(0, viewportHeight, 0);
-        lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focalLength);
+        origin = lookFrom;
+        horizontal = viewportWidth * u;
+        vertical = viewportHeight * v;
+        lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - w;
     }
 
-    ray getRay(double u, double v) const {
-        return ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+    ray getRay(double s, double t) const {
+        return ray(origin, lowerLeftCorner + s * horizontal + t * vertical - origin);
     }
 
 private:
--- main.cpp
+++ main.cpp
@@ -47,15 +47,20 @@ int main() {
     const auto R = cos(pi / 4);
     hittableList world;
 
-    const auto material_left = make_shared<lambertian>(color(0, 0, 1));
-    const auto material_right = make_shared<lambertian>(color(1, 0, 0));
+    const auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    const auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    const auto material_left = make_shared<dielectric>(1.5);
+    const auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-    world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
 
-    camera cam(90.0, aspectRatio);
+    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspectRatio);
 
     // Render to PPM (Portable Pix Map) image format
 
